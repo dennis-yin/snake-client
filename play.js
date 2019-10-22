a6bb52bb9091e379eac2c97ff8e07e5da04c2306
dennis-yin <dennis.yin26@gmail.com>
2019-10-22 23:17:11 +0000

    adding ability to exit out of program

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 const { connect } = require('./client');
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
 console.log('Connecting ...');
 
 connect();
+
+setupInput();
