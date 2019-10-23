bb4452fb0977469c0bea3040c4901102e6318a8a
dennis-yin <dennis.yin26@gmail.com>
2019-10-23 00:15:17 +0000

    making hardcoded values into consts

--- input.js
+++ input.js
@@ -1,14 +1,41 @@
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// Stores the active TCP connection object.
+let connection;
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
+
+  const handleUserInput = function(key) {
+
+    if (key === 'x') {
+      connection.write('Say: YO!');
+    }
+
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
+
+    if (key === '\u0003') {
+      process.exit();
+    }
+
+  };
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
